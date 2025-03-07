065db8898f5ecf871dcca32bae9d243d17484832
Arjun V <arjunvnd@gmail.com>
2025-03-07 10:04:44 +0530

    added header and footer

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>test</Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
