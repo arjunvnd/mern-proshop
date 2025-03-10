81f4e4e30357aaa9724b5f862fade8a251e48977
Arjun V <arjunvnd@gmail.com>
2025-03-10 10:32:23 +0530

    updated pdp page

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -6,7 +6,7 @@ import Rating from "./Rating";
 const Products = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <Link to={`/products/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
       </Link>
       <CardBody>
